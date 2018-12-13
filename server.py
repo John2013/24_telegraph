b92bf78b7fc855c2dd6060322028d60ed3ba7391
Evgeny Timofeyev <nawasington@ya.ru>
2018-12-13 13:09:16 +0300

    cookie статей, кнопка изменить

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import json
+import uuid
 from datetime import date
 from os.path import exists
 
 import bleach
 from bleach_whitelist import markdown_tags, markdown_attrs
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, make_response
 from markdown import markdown
 
 app = Flask(__name__)
@@ -42,12 +43,13 @@ def get_article(slug):
         return False
 
     with open(file_path, 'r', encoding="utf-8") as file:
-        header, signature, body, ordinal_date = json.load(file)
+        header, signature, body, ordinal_date, article_id = json.load(file)
     return {
         'header': header,
         'signature': signature,
         'body': body,
-        'date': date.fromordinal(ordinal_date)
+        'date': date.fromordinal(ordinal_date),
+        'id': article_id
     }
 
 
@@ -55,25 +57,34 @@ def get_article(slug):
 def form():
     if request.method == 'POST':
         cur_date = date.today()
+        article_id = str(uuid.uuid4())
         article = [
             request.form['header'],
             request.form['signature'],
             request.form['body'],
-            cur_date.toordinal()
+            cur_date.toordinal(),
+            article_id
         ]
         file_path, slug = get_article_file_path_and_slug(
             request.form['header']
         )
         with open(file_path, 'w', encoding="utf-8") as file:
             json.dump(article, file, ensure_ascii=False)
-        return redirect('/{}'.format(slug))
 
-    return render_template('form.html')
+        article_ids = json.loads(request.cookies.get('articles') or "[]")
+
+        article_ids.append(article_id)
+        responce = make_response(redirect('/{}'.format(slug)))
+        responce.set_cookie('articles', json.dumps(article_ids))
+        return responce
+
+    return render_template('form-create.html')
 
 
 @app.route('/<string:slug>')
 def article_page(slug):
     article = get_article(slug)
+
     if article is False:
         return render_template('404.html'), 404
 
@@ -82,6 +93,9 @@ def article_page(slug):
         markdown_tags,
         markdown_attrs
     )
+    article_ids = json.loads(request.cookies.get('articles'))
+    if article['id'] in article_ids:
+        return render_template('article-edit.html', article=article, slug=slug)
 
     return render_template('article.html', article=article)
 
